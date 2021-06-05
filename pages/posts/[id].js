07995d170a08d2de4f741586bdc8f4a65a084d05
Artem Novichkov <novichkoff93@gmail.com>
2021-06-05 12:26:55 +0600

    Update logic

---
+++ pages/posts/[id].js
@@ -0,0 +1,39 @@
+import Layout from '../../components/layout'
+import Date from '../../components/date'
+import Head from 'next/head'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
+
+export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id)
+    return {
+        props: {
+            postData
+        }
+    }
+}
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            <Head>
+            <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{__html: postData.htmlContent}} />
+            </article>
+        </Layout>
+    )
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
