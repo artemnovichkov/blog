15c36107d7c66bde857028a3cb69285bbe12c62d
Artem Novichkov <novichkoff93@gmail.com>
2021-06-05 12:48:33 +0600

    Add code highlight

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,6 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Bearded Swift developer from Siberia 👨🏻‍💻</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
