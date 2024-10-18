f524324439fc0549c0a1197a91f4939fa918b305
Artem Novichkov <mail@artemnovichkov.com>
2024-10-18 20:58:08 +0500

    Return plugins

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,10 +1,13 @@
-
+import remarkRehype from 'remark-rehype'
+import rehypeRaw from 'rehype-raw'
 import remarkHtml from 'remark-html'
 import { remark } from 'remark'
 
 export default async function markdownToHtml(markdown: string) {
     const result = await remark()
         .use(remarkHtml)
+        .use(remarkRehype, { allowDangerousHtml: true })
+        .use(rehypeRaw)
         .process(markdown)
 
     return result.toString();
