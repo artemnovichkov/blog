1385062d78a1a60a52ef85a09c66068c0fede9e9
Artem Novichkov <mail@artemnovichkov.com>
2024-11-17 14:16:02 +0500

    Add support of line highlighting and Tweet component

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,26 +1,30 @@
+import { useMDXComponents } from "../mdx-components";
+import { compileMDX } from "next-mdx-remote/rsc";
+import rehypeSlug from "rehype-slug";
+import remarkGfm from "remark-gfm";
 import rehypeShiki from '@shikijs/rehype'
-import rehypeStringify from 'rehype-stringify'
-import remarkRehype from 'remark-rehype'
-import rehypeRaw from 'rehype-raw'
-import remarkHtml from 'remark-html'
-import { remark } from 'remark'
-import remarkGfm from 'remark-gfm'
-export default async function markdownToHtml(markdown: string) {
-    const result = await remark()
-        .use(remarkHtml)
-        .use(remarkGfm)
-        .use(remarkRehype, { allowDangerousHtml: true })
-        .use(rehypeRaw)
-        .use(rehypeShiki, {
-            themes: {
-                light: 'github-dark',
-                dark: 'github-dark',
-            },
-            inline: 'tailing-curly-colon',
-            defaultLanguage: 'swift',
-        })
-        .use(rehypeStringify)
-        .process(markdown)
+import { transformerNotationHighlight } from 'shikiji-transformers'
 
-    return result.toString();
+export default async function markdownToHtml(markdown: string) {
+    const components = useMDXComponents({});
+
+    const { content, frontmatter } = await compileMDX({
+        source: markdown,
+        options: {
+            mdxOptions: {
+                rehypePlugins: [
+                    [rehypeShiki, {
+                        theme: 'github-dark',
+                        transformers: [transformerNotationHighlight()],
+                        inline: 'tailing-curly-colon',
+                        defaultLanguage: 'swift',
+                    }],
+                    rehypeSlug],
+                remarkPlugins: [remarkGfm],
+            },
+        },
+        components,
+    });
+
+    return content;
 }
---
+++ src/mdx-components.tsx
@@ -0,0 +1,9 @@
+import type { MDXComponents } from 'mdx/types'
+import { Tweet } from 'react-tweet'
+ 
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    ...components,
+    Tweet,
+  }
+}
