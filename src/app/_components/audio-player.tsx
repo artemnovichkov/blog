9af627d8839f7a627ac1480ee841bb835ea9bd7d
Artem Novichkov <mail@artemnovichkov.com>
2025-05-01 15:00:04 +0500

    Add audio player

---
+++ src/app/_components/audio-player.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface AudioPlayerProps {
+  src: string;
+}
+
+const AudioPlayer: React.FC<AudioPlayerProps> = ({ src }) => {
+  return (
+    <div className="audio-player p-4 rounded-md flex flex-col gap-2 bg-gray-100 border border-gray-300 dark:bg-gray-800 dark:border-gray-600 w-full">
+      <div>
+        <p>
+          Prefer audio format? Listen to the blog post below:
+        </p>
+        <audio controls src={src} className="w-full">
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    </div>
+  );
+};
+
+export default AudioPlayer; 
