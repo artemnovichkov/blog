521ffc7bc1af5ca0848f7c013d9ff376b5f3045b
Artem Novichkov <mail@artemnovichkov.com>
2025-05-02 16:41:55 +0500

    Fix page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -6,9 +6,42 @@ import Image from 'next/image'
 import type { Metadata } from 'next'
 import { name } from '@/lib/const'
 
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
-    const { slug } = await params;
-    const post = getPostBySlug(slug);
+export default async function BlogPost(props: Params) {
+    const params = await props.params;
+    const post = getPostBySlug(params.slug);
+    const content = await markdownToHtml(post.content || "");
+    return (
+        <div>
+            <article>
+                <h1 className="mb-4 font-bold text-3xl tracking-tight text-zinc-800 dark:text-gray-100">
+                    {post.title}
+                </h1>
+                <PostHeader post={post} />
+                <Image className="mb-4 rounded"
+                    priority
+                    alt={post.title}
+                    src={post.cover}
+                    width={1200}
+                    height={740}
+                />
+                <div className="prose dark:prose-dark">
+                  {content}
+                </div>
+            </article>
+            <PostActions post={post} />
+        </div>
+    );
+}
+
+type Params = {
+    params: Promise<{
+      slug: string;
+    }>;
+  };
+
+export async function generateMetadata(props: Params): Promise<Metadata> {
+    const params = await props.params;
+    const post = getPostBySlug(params.slug);
     return {
         title: post.title,
         description: post.description,
@@ -38,37 +71,10 @@ export async function generateMetadata({ params }: { params: { slug: string } })
     };
 }
 
-export async function generateStaticParams(): Promise<{ slug: string }[]> {
+export async function generateStaticParams() {
     const posts = getAllPosts();
-
+  
     return posts.map((post) => ({
-        slug: post.slug,
+      slug: post.slug,
     }));
-}
-
-export default async function BlogPost({ params }: { params: { slug: string } }) {
-    const { slug } = await params;
-    const post = getPostBySlug(slug);
-    const content = await markdownToHtml(post.content || "");
-    return (
-        <div>
-            <article>
-                <h1 className="mb-4 font-bold text-3xl tracking-tight text-zinc-800 dark:text-gray-100">
-                    {post.title}
-                </h1>
-                <PostHeader post={post} />
-                <Image className="mb-4 rounded"
-                    priority
-                    alt={post.title}
-                    src={post.cover}
-                    width={1200}
-                    height={740}
-                />
-                <div className="prose dark:prose-dark">
-                  {content}
-                </div>
-            </article>
-            <PostActions post={post} />
-        </div>
-    );
 }
