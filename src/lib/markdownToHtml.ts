e4c019548f7ae9eafbfefeb172893617c5d2d3dd
Artem Novichkov <mail@artemnovichkov.com>
2025-05-15 18:42:17 +0500

    Replace shiki with ehype-pretty-code

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,12 +1,17 @@
 import { compileMDX } from "next-mdx-remote/rsc";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
-import rehypeShiki from '@shikijs/rehype'
-import { transformerNotationHighlight } from 'shikiji-transformers'
+import rehypePrettyCode from "rehype-pretty-code";
 import { Tweet } from 'react-tweet'
 import Callout from '../app/_components/callout';
 import { FileTree } from "../app/_components/filetree";
 import AudioPlayer from '../app/_components/audio-player';
+
+
+const options = {
+    theme: 'github-dark',
+    keepBackground: false,
+}
 
 export default async function markdownToHtml(markdown: string) {
     const { content } = await compileMDX({
@@ -14,12 +19,7 @@ export default async function markdownToHtml(markdown: string) {
         options: {
             mdxOptions: {
                 rehypePlugins: [
-                    [rehypeShiki, {
-                        theme: 'github-dark',
-                        transformers: [transformerNotationHighlight()],
-                        inline: 'tailing-curly-colon',
-                        defaultLanguage: 'swift',
-                    }],
+                    [rehypePrettyCode, options],
                     rehypeSlug],
                 remarkPlugins: [remarkGfm],
             },
