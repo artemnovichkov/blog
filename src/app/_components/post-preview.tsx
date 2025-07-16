76647a219540ed49af1a95abdbfb7224ea52160a
Artem Novichkov <mail@artemnovichkov.com>
2025-07-16 16:04:03 +0500

    Fix link issue

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -11,32 +11,34 @@ interface PostPreviewProps {
 
 export default function PostPreview({ post }: PostPreviewProps) {
     return (
-        <Link href={`/blog/${post.slug}`}>
-            <div className="mb-4 flex items-center dark:bg-gray-800 p-4 rounded-lg border border-zinc-300 dark:border-gray-600">
-                <Image className="rounded mr-4 w-1/4 sm:w-52"
-                    priority
-                    width={200}
-                    height={124}
-                    src={post.cover}
-                    alt={`cover`}
-                />
-                <div className="flex flex-col justify-between min-h-32">
-                    <div>
+        <div className="mb-4 flex items-center dark:bg-gray-800 p-4 rounded-lg border border-zinc-300 dark:border-gray-600">
+            <Link className="mr-4 flex-shrink-0" href={`/blog/${post.slug}`}>
+                    <Image className="rounded"
+                        priority
+                        width={200}
+                        height={124}
+                        src={post.cover}
+                        alt={`cover`}
+                    />
+                </Link>
+            <div className="flex flex-col justify-between min-h-32">
+                <div>
+                    <Link href={`/blog/${post.slug}`}>
                         <h4 className="text-xl font-medium text-zinc-800 dark:text-gray-100">
                             {post.title}
                         </h4>
-                        <h4 className="mb-4 text-base font-normal text-zinc-500 dark:text-gray-400">
-                            {post.description}
-                        </h4>
-                        {post.categories && <PostCategories categories={post.categories} />}
-                    </div>
-                    <div className="text-sm font-normal font-xs text-zinc-500 dark:text-gray-400">
-                        <PostDate dateString={post.date} />
-                        {` • `}
-                        {readingTime(post.content).text}
-                    </div>
+                    </Link>
+                    <h4 className="mb-4 text-base font-normal text-zinc-500 dark:text-gray-400">
+                        {post.description}
+                    </h4>
+                    {post.categories && <PostCategories categories={post.categories} />}
+                </div>
+                <div className="text-sm font-normal font-xs text-zinc-500 dark:text-gray-400">
+                    <PostDate dateString={post.date} />
+                    {` • `}
+                    {readingTime(post.content).text}
                 </div>
             </div>
-        </Link>
+        </div>
     )
 }
