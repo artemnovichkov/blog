aeda5d88bf0489542dd0bb594bc24e9871d9c506
Artem Novichkov <mail@artemnovichkov.com>
2025-07-16 17:23:01 +0500

    Fix footer position

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,9 +40,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-zinc-100 dark:bg-gray-900">
+      <body className="min-h-screen flex flex-col bg-zinc-100 dark:bg-gray-900">
         <Header />
-        <main className="flex flex-col justify-center max-w-2xl mx-auto px-4 sm:px-0">
+        <main className="flex-1 flex flex-col max-w-2xl mx-auto px-4 sm:px-0">
           {children}
         </main>
         <Footer />
