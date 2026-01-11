af22dd2d3040d5c7e402a4884354cf18d0bd5758
Artem Novichkov <mail@artemnovichkov.com>
2026-01-11 11:23:56 +0500

    Revert "Add PointFree ad"
    This reverts commit 7cfc24e47a71fc265b7ac488938548365acf4767.

--- src/lib/sponsorship-config.ts
+++ src/lib/sponsorship-config.ts
@@ -7,8 +7,8 @@ export interface SponsorshipConfig {
 
 export const sponsorshipConfig: SponsorshipConfig = {
   isVisible: true,
-  title: "Point-Free End-of-Year Sale: 25% Off",
+  title: "Sponsor This Blog",
   description:
-    "Get 25% off 1 year of Point-Free! Access hundreds of hours of advanced Swift content: SQLite persistence, concurrency, app architecture, cross-platform development. Limited time only!",
-  url: "https://www.pointfree.co/discounts/eoy-2025?utm_source=artemnovichkov&utm_medium=blog&utm_campaign=eoy-2025",
+    "Connect with a targeted audience of iOS developers and Swift engineers. Promote your developer tools, courses, or services.",
+  url: "https://artemnovichkov.com/sponsorship",
 }
